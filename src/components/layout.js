72516dc7bbe49d8f5c8ab42207c181211697b88e
malmosands <matthias@c1x.dev>
2020-08-16 22:27:07 +0200

    remove header

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,8 +10,13 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import Container from "react-bootstrap/Container"
+import Form from "react-bootstrap/Form"
+import FormControl from "react-bootstrap/FormControl"
 import "../styles/style.css"
+import Countries from "../assets/countries.json"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -31,7 +36,22 @@ const Layout = ({ children }) => {
           class: ''
         }}
       />
-      <Header />
+      <Navbar bg="transparent" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand className="mr-0"><span className="font-weight-light">CountryCodes</span><span className="font-weight-normal">DB</span></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Form className="w-100 mx-5">
+              <FormControl type="text" placeholder="Search for a Country..." className="w-100 border-0 shadow-lg" />
+            </Form>
+            <Nav className="ml-auto">
+              <Navbar.Text className="text-truncate">
+                Country Data for {Countries.length} Countries
+              </Navbar.Text>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div>
         <main>{children}</main>
         <footer>
